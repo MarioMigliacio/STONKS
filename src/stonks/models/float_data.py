ba007e8eba4a24bb65968494fc60b52a86cc15d7
Mario <migliacio@gmail.com>
2026-09-10 20:56:21 -0700

    added __str__ for dataclasses and updated main slightly. Turned on the float config setting and verified execution.

--- src/stonks/models/float_data.py
+++ src/stonks/models/float_data.py
@@ -9,10 +9,36 @@ from datetime import date
 
 @dataclass
 class FloatData:
-    """Represents reported public-float data for a stock."""
+    """
+    Represents reported public-float data for a stock.
+
+    Attributes:
+        symbol:
+            Stock ticker symbol.
+
+        float_shares:
+            Physical count of shares as reported by API.
+
+        float_percent:
+            Percentage of market float as reported by API.
+
+        effective_date:
+            From the date of which the Float data was reported at.
+
+        source:
+            Originating source of data, string format.
+    """
 
     symbol: str
     float_shares: int
     float_percent: float
     effective_date: date
     source: str
+
+    def __str__(self) -> str:
+        return (
+            f"Float: {self.float_shares:,} | "
+            f"Float %: {self.float_percent:.2f}% | "
+            f"Effective: {self.effective_date} | "
+            f"Source: {self.source}"
+        )
